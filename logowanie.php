094fbfe7565bd71692647c02a2adf0ea7c8a1831
Tomasz Diduszko <tominator1@wp.pl>
2015-10-16 07:38:18 +0200

    Logowanie bez bootstrapa. Tym razem poprawny.

--- logowanie.php
+++ logowanie.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="pl">
   <head>
@@ -34,33 +33,3 @@
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
-=======
-<?php
-if(isset($_POST['login']))
-{
-	$login = $_POST['login'];
-	$haslo = $_POST['haslo'];
-	
-	$p = polacz();
-	
-	$zapytanie = "SELECT COUNT(*) FROM users WHERE Login LIKE '".$login."' AND Pass LIKE '".$haslo."';";
-	$res = $p->query($zapytanie);
-	$row = $res->fetch_array(MYSQL_NUM);
-	
-	if($row[0] == 1)
-	{
-		$_SESSION['zalogowany'] = 1;
-		$zapytanie = "SELECT * FROM users WHERE Login LIKE '".$login."' AND Pass LIKE '".$haslo."';";
-		$res = $p->query($zapytanie);
-		$row = $res->fetch_assoc();
-		$_SESSION['id_user'] = $row['ID'];
-		$_SESSION['permission'] = $row['PERMISSIONS_ID'];
-		header("Location: index.php");
-	}
-	else
-	{
-		echo "Niepoprawny login lub haslo";
-	}
-}
-?>
->>>>>>> a7a62e6566ac6206f7b5a628a5ee6de6eff1131a
--- polaczZBD.php
+++ polaczZBD.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Łączenie z BD
 	function polacz()
@@ -33,38 +32,3 @@ session_destroy();
 echo "Zostałeś wylogowany";
 echo "<a href='logowanie.php'>Wróć na stronę logowania</a>"*/
 ?>
-=======
-<?php
-// Łączenie z BD
-	function polacz()
-	{
-		$p = new mysqli("localhost", "root", "", "tdstrona");
-		
-		if($p->connect_errno)
-		{
-			echo "Błąd logowania z bazą";
-		}
-		else
-		{
-			return $p;
-		}
-	}
-	
-	function rozlacz()
-	{
-		mysqli_close($p);
-	}
-?>
-
-<?php
-	/*header("index.php")
-?>
-
-<?php
-//Wylogowanie
-session_start();
-session_destroy();
-echo "Zostałeś wylogowany";
-echo "<a href='logowanie.php'>Wróć na stronę logowania</a>"*/
-?>
->>>>>>> a7a62e6566ac6206f7b5a628a5ee6de6eff1131a
